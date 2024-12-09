8f5e07ce770a74b48511a99c06b702aa21c942ca
marjanhasan <moonmarjanhasan@gmail.com>
2024-12-09 19:17:27 +0600

    menu page added

--- src/pages/Menu/Menu/Menu.jsx
+++ src/pages/Menu/Menu/Menu.jsx
@@ -1,12 +1,61 @@
 import { Helmet } from "react-helmet-async";
+import Cover from "../../Shared/Cover/Cover";
+import banner from "../../../assets/menu/banner3.jpg";
+import dessert from "../../../assets/menu/dessert-bg.jpeg";
+import pizza from "../../../assets/menu/pizza-bg.jpg";
+import salad from "../../../assets/menu/salad-bg.jpg";
+import soup from "../../../assets/menu/soup-bg.jpg";
+import PopularMenu from "../../Home/PopularMenu/PopularMenu";
 
 const Menu = () => {
   return (
-    <div>
+    <div className="max-w-screen-xl mx-auto">
       <Helmet>
         <title>Restauran App - Menu</title>
       </Helmet>
-      <h1>hello menu</h1>
+      <Cover
+        img={banner}
+        title={"our menu"}
+        description={"would you like to try a dish?"}
+        margin={"mb-6"}
+      />
+      <PopularMenu />
+      <Cover
+        img={dessert}
+        title={"desserts"}
+        description={
+          "Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+        }
+        margin={"mt-6"}
+      />
+      <PopularMenu />
+      <Cover
+        img={pizza}
+        title={"pizza"}
+        description={
+          "Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+        }
+        margin={"mt-6"}
+      />
+      <PopularMenu />
+      <Cover
+        img={salad}
+        title={"salad"}
+        description={
+          "Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+        }
+        margin={"mt-6"}
+      />
+      <PopularMenu />
+      <Cover
+        img={soup}
+        title={"soup"}
+        description={
+          "Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+        }
+        margin={"mt-6"}
+      />
+      <PopularMenu />
     </div>
   );
 };
---
+++ src/pages/Shared/Cover/Cover.jsx
@@ -0,0 +1,18 @@
+const Cover = ({ img, title, description, margin = "" }) => {
+  return (
+    <section
+      className={`relative h-[600px] w-full ${margin} z-10 flex justify-center items-center`}
+    >
+      <div
+        className={`absolute h-full w-full bg-cover bg-center bg-no-repeat -z-10`}
+        style={{ backgroundImage: `url(${img})` }}
+      ></div>
+      <div className="uppercase w-[75%] h-[40%] flex flex-col justify-center mx-auto my-auto bg-[#151515] text-center opacity-70">
+        <h2 className="text-4xl z-10 text-white">{title}</h2>
+        <p className="text-sm mt-2 px-10">{description}</p>
+      </div>
+    </section>
+  );
+};
+
+export default Cover;
