24b41323faf7ded13ffc05998d10c7748d2a013b
marjanhasan <moonmarjanhasan@gmail.com>
2024-12-17 17:58:01 +0600

    Login page added

--- src/layouts/Main.jsx
+++ src/layouts/Main.jsx
@@ -1,13 +1,17 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import Footer from "../pages/Shared/Footer/Footer";
 import Navbar from "../pages/Shared/Navbar/Navbar";
 
 const Main = () => {
+  const location = useLocation().pathname;
+  const noNavbarFooter =
+    location.includes("/login") || location.includes("/signup");
+
   return (
     <>
-      <Navbar />
+      {noNavbarFooter || <Navbar />}
       <Outlet />
-      <Footer />
+      {noNavbarFooter || <Footer />}
     </>
   );
 };
