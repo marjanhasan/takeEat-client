5b735780d2f61ee717a19ab39774fdc74e2b1a65
marjanhasan <moonmarjanhasan@gmail.com>
2024-12-18 21:18:15 +0600

    firebase added

--- src/main.jsx
+++ src/main.jsx
@@ -5,13 +5,16 @@ import { RouterProvider } from "react-router-dom";
 import { router } from "./routes/Routes";
 import "@smastrom/react-rating/style.css";
 import { HelmetProvider } from "react-helmet-async";
+import AuthProviders from "./providers/AuthProviders";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <HelmetProvider>
-      <div className="bg-gray-800">
-        <RouterProvider router={router} />
-      </div>
-    </HelmetProvider>
+    <AuthProviders>
+      <HelmetProvider>
+        <div className="bg-gray-800">
+          <RouterProvider router={router} />
+        </div>
+      </HelmetProvider>
+    </AuthProviders>
   </StrictMode>
 );
