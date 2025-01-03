7a9a495f40f117b493a0704c67851d74dfffee88
marjanhasan <moonmarjanhasan@gmail.com>
2025-01-03 20:09:40 +0600

    under construction alert added

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -4,8 +4,20 @@ import Category from "../Category/Category";
 import Featured from "../Featured/Featured";
 import PopularMenu from "../PopularMenu/PopularMenu";
 import Testimonials from "../Testimonials/Testimonials";
+import { useEffect } from "react";
+import Swal from "sweetalert2";
 
 const Home = () => {
+  useEffect(() => {
+    Swal.fire({
+      position: "center",
+      icon: "info",
+      title: `This site is still under construction.`,
+      text: "Sorry for the minor errors and responsive issues. I will fix it soon!",
+      showConfirmButton: false,
+      timer: 3000,
+    });
+  }, []);
   return (
     <div className="max-w-screen-xl mx-auto">
       <Helmet>
